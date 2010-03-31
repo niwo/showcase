6fac5cbf7f6e63320f807dcab260d88b7f08cb62
Your Name <nik.wolfgramm@gmail.com>
2010-03-31 02:43:39 +0200

    First import

---
+++ showcase.rb
@@ -0,0 +1,73 @@
+# Showcase Webapplication
+# (C) 2010 Nik Wolfgramm
+
+require 'rubygems'
+require 'sinatra'
+require 'yaml'
+require 'haml'
+require 'unicode'
+require 'sinatra/r18n'
+ 
+set :haml, {:format => :html5 }
+ 
+before do
+  headers "Content-Type" => "text/html charset=utf8"
+end
+
+helpers do
+  def language_select(page)
+    lang_sel = ""
+    page += '/' if page 
+    r18n.available_locales.each do |locale| 
+      lang_sel += r18n.locale.code == locale.code ? locale.title  : "<a href='/#{locale.code}/#{page}'>#{locale.title}</a>"
+      lang_sel += " | " unless( locale.code == r18n.available_locales[-1].code)
+    end
+    lang_sel
+  end
+  
+  def menu_links(page = 'home')
+    links = {'home' => '/', 'resume' => '/resume/', 'portfolio' => '/portfolio/'}
+    menu = ""
+    links.each_pair do |name, link|
+      menu += (page == name) ? t.showcase.send(name) : "<a href='/#{r18n.locale.code}#{link}'>#{t.showcase.send(name)}</a>"
+      menu += " | " unless( link == link[-1])
+    end
+    menu
+  end
+  
+  def text_with_line_breaks(text)
+    text.gsub("\n", "<br />")
+  end
+  
+  def obscure_email(email)
+    return nil if email.nil?
+    lower = ('a'..'z').to_a
+    upper = ('A'..'Z').to_a
+    email.split('').map { |char|
+        output = lower.index(char) + 97 if lower.include?(char)
+        output = upper.index(char) + 65 if upper.include?(char)
+        output ? "&##{output};" : (char == '@' ? '&#0064;' : char)
+    }.join
+  end
+end
+
+get '/:locale?/?' do
+  haml :index
+end
+ 
+get '/:locale/resume/?' do
+  @page = 'resume'
+  resumes = YAML.load_file( './data/resumes.yml' )
+  @personal = YAML.load_file( './data/personal.yml' )
+  @language = r18n.locale.code
+  @page_title = resumes[@language]['title']
+  @resume = resumes[@language]
+  haml :resume
+end
+ 
+get '/:locale/portfolio/?' do
+  @page = 'portfolio'
+  @page_title = "Project Portfolio"
+  @projects = YAML.load_file( './data/projects.yml' )
+  haml :portfolio
+end
